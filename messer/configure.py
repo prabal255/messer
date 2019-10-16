ab48030c6c41217e6a83456a30ee0e3664f45f21
prabal255 <51309311+prabal255@users.noreply.github.com>
2019-10-16 19:27:37 +0530

    Update configure.py

--- messer/configure.py
+++ messer/configure.py
@@ -158,3 +158,18 @@ def configure_messer_azure(args):
 
 def configure_messer_aws(args):
     return AWSConfigure(args)
+
+
+'''
+(c) 2018 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+
+"""
+
